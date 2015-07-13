7e59342db9cbcace1456f89dc44b1eba36e584eb
Eneko <contact@eneko.me>
2015-07-14 01:51:50 +0200

    Identation changed from 4 spaces to 2 spaces.

--- index.js
+++ index.js
@@ -17,72 +17,74 @@ var PLUGIN_NAME = 'gulp-hogan-precompile';
  */
 module.exports = function compile(options) {
 
-    var templates = {};
+  var templates = {};
 
-    // Plugin and hogan options
-    options = extend(true, {
-        newLine: gutil.linefeed,
-        wrapper: 'amd',
-        templateOptions: {},
-        templateName: function(file) {
-            return path.join(
-                path.dirname(file.relative),
-                path.basename(file.relative, path.extname(file.relative))
-            );
-        },
-        hoganModule: 'hogan'
-    }, options || {});
+  // Plugin and hogan options
+  options = extend(true, {
+    newLine: gutil.linefeed,
+    wrapper: 'amd',
+    templateOptions: {},
+    templateName: function(file) {
+      return path.join(
+        path.dirname(file.relative),
+        path.basename(file.relative, path.extname(file.relative))
+      );
+    },
+    hoganModule: 'hogan'
+  }, options || {});
 
-    // Do not convert to strings if dest is an object
-    options.templateOptions.asString = true;
+  // Do not convert to strings if dest is an object
+  options.templateOptions.asString = true;
 
-    return through.obj(function (file, enc, callback) {
+  return through.obj(function (file, enc, callback) {
 
-        if (file.isNull()) {
-            return callback(null, file);
-        }
+    if (file.isNull()) {
+      return callback(null, file);
+    }
 
-        if (file.isStream()) {
-            this.emit('error', new PluginError(PLUGIN_NAME,  'Streaming not supported'));
-            return callback();
-        }
+    if (file.isStream()) {
+      this.emit('error', new PluginError(PLUGIN_NAME,  'Streaming not supported'));
+      return callback();
+    }
 
-        templates[options.templateName(file)] = hogan.compile(file.contents.toString('utf8'), options.templateOptions);
+    templates[options.templateName(file)] = hogan.compile(file.contents.toString('utf8'),
+                                                          options.templateOptions);
 
-        var lines = [],
-            blanks = options.wrapper ? '    '
-                                     : '';
+    var lines = [],
+        blanks = options.wrapper ? '  '
+                   : '';
 
-        for (var name in templates) {
-            lines.push(blanks + 'templates[\'' + name + '\'] = new Hogan.Template(' + templates[name] + ');');
-        }
+    for (var name in templates) {
+      lines.push(blanks + 'templates[\'' + name + '\'] = new Hogan.Template('
+                 + templates[name] + ');');
+    }
 
-        // Unwrapped
-        lines.unshift('    var templates = {};');
+    // Unwrapped
+    lines.unshift('  var templates = {};');
 
-        // All wrappers require a hogan module
-        if (options.wrapper) {
-            lines.unshift('    var Hogan = require(\'' + options.hoganModule  + '\');');
-            lines.push('    return templates;');
-        }
+    // All wrappers require a hogan module
+    if (options.wrapper) {
+      lines.unshift('  var Hogan = require(\'' + options.hoganModule  + '\');');
+      lines.push('  return templates;');
+    }
 
-        // AMD wrapper
-        if (options.wrapper === 'amd') {
-            lines.unshift('define(function(require) {');
-            lines.push('})');
-        }
+    // AMD wrapper
+    if (options.wrapper === 'amd') {
+      lines.unshift('define(function(require) {');
+      lines.push('})');
+    }
 
-        // CommonJS wrapper
-        else if (options.wrapper === 'commonjs') {
-            lines.unshift('module.exports = (function() {');
-            lines.push('})();');
-        }
+    // CommonJS wrapper
+    else if (options.wrapper === 'commonjs') {
+      lines.unshift('module.exports = (function() {');
+      lines.push('})();');
+    }
 
-        file.contents = new Buffer(lines.join(options.newLine));
-        file.path = gutil.replaceExtension(file.path, '.js');
+    file.contents = new Buffer(lines.join(options.newLine));
+    file.path = gutil.replaceExtension(file.path, '.js');
 
-        callback(null, file);
+    callback(null, file);
 
-    });
+  });
 
 };
--- test/main.js
+++ test/main.js
@@ -9,76 +9,76 @@ var Buffer = require('buffer').Buffer;
 
 describe('gulp-hogan-precompile', function() {
 
-    function getFakeFile(path, content) {
-        return new File({
-            cwd: '',
-            base: 'test/',
-            path: path,
-            contents: new Buffer(content)
-        });
-    }
-
-    describe('compile()', function() {
-
-        it('should compile templates', function(done) {
-            var stream = compile();
-            stream.on('data', function(newFile) {
-                should.exist(newFile);
-                should.exist(newFile.contents);
-                path.basename(newFile.path).should.equal('file.js');
-                Buffer.isBuffer(newFile.contents).should.equal(true);
-                done();
-            });
-            stream.write(getFakeFile('test/file.html', 'hello {{place}}'));
-            stream.end();
-        });
-
-        it('should use options.newLine', function(done) {
-            var stream = compile({
-                newLine: '\r\n'
-            });
-            stream.on('data', function(newFile){
-                var lines = newFile.contents.toString().split('\r\n');
-                lines.length.should.equal(6);
-                done();
-            });
-            stream.write(getFakeFile('test/file.html', 'hello {{place}}'));
-            stream.end();
-        });
-
-        it('should compile string templates to amd modules', function(done) {
-            var stream = compile();
-            stream.on('data', function(newFile){
-                var lines = newFile.contents.toString().split(gutil.linefeed);
-                lines[0].should.equal('define(function(require) {');
-                lines[1].should.equal('    var Hogan = require(\'hogan\');');
-                lines[2].should.equal('    var templates = {};');
-                lines[3].should.match(/templates\['file'\] = new Hogan.Template/);
-                lines.pop().should.equal('})');
-                lines.pop().should.equal('    return templates;');
-                done();
-            });
-            stream.write(getFakeFile('test/file.html', 'hello {{place}}'));
-            stream.end();
-        });
-
-        it('should compile string templates to commonjs modules', function(done) {
-            var stream = compile({
-                wrapper: 'commonjs'
-            });
-            stream.on('data', function(newFile){
-                var lines = newFile.contents.toString().split(gutil.linefeed);
-                lines[0].should.equal('module.exports = (function() {');
-                lines[1].should.equal('    var Hogan = require(\'hogan\');');
-                lines[2].should.equal('    var templates = {};');
-                lines[3].should.match(/templates\['file'\] = new Hogan.Template/);
-                lines.pop().should.equal('})();');
-                lines.pop().should.equal('    return templates;');
-                done();
-            });
-            stream.write(getFakeFile('test/file.html', 'hello {{place}}'));
-            stream.end();
-        });
-
+  function getFakeFile(path, content) {
+    return new File({
+      cwd: '',
+      base: 'test/',
+      path: path,
+      contents: new Buffer(content)
     });
+  }
+
+  describe('compile()', function() {
+
+    it('should compile templates', function(done) {
+      var stream = compile();
+      stream.on('data', function(newFile) {
+        should.exist(newFile);
+        should.exist(newFile.contents);
+        path.basename(newFile.path).should.equal('file.js');
+        Buffer.isBuffer(newFile.contents).should.equal(true);
+        done();
+      });
+      stream.write(getFakeFile('test/file.html', 'hello {{place}}'));
+      stream.end();
+    });
+
+    it('should use options.newLine', function(done) {
+      var stream = compile({
+        newLine: '\r\n'
+      });
+      stream.on('data', function(newFile){
+        var lines = newFile.contents.toString().split('\r\n');
+        lines.length.should.equal(6);
+        done();
+      });
+      stream.write(getFakeFile('test/file.html', 'hello {{place}}'));
+      stream.end();
+    });
+
+    it('should compile string templates to amd modules', function(done) {
+      var stream = compile();
+      stream.on('data', function(newFile){
+        var lines = newFile.contents.toString().split(gutil.linefeed);
+        lines[0].should.equal('define(function(require) {');
+        lines[1].should.equal('  var Hogan = require(\'hogan\');');
+        lines[2].should.equal('  var templates = {};');
+        lines[3].should.match(/templates\['file'\] = new Hogan.Template/);
+        lines.pop().should.equal('})');
+        lines.pop().should.equal('  return templates;');
+        done();
+      });
+      stream.write(getFakeFile('test/file.html', 'hello {{place}}'));
+      stream.end();
+    });
+
+    it('should compile string templates to commonjs modules', function(done) {
+      var stream = compile({
+        wrapper: 'commonjs'
+      });
+      stream.on('data', function(newFile){
+        var lines = newFile.contents.toString().split(gutil.linefeed);
+        lines[0].should.equal('module.exports = (function() {');
+        lines[1].should.equal('  var Hogan = require(\'hogan\');');
+        lines[2].should.equal('  var templates = {};');
+        lines[3].should.match(/templates\['file'\] = new Hogan.Template/);
+        lines.pop().should.equal('})();');
+        lines.pop().should.equal('  return templates;');
+        done();
+      });
+      stream.write(getFakeFile('test/file.html', 'hello {{place}}'));
+      stream.end();
+    });
+
+  });
 });
